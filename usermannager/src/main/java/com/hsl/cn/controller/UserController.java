0506f34252ae72aa893dc658b4277c4eb2f0e2ba
tester <2275057986@qq.com>
2019-06-17 17:58:37 +0800

    修改bug

--- usermannager/src/main/java/com/hsl/cn/controller/UserController.java
+++ usermannager/src/main/java/com/hsl/cn/controller/UserController.java
@@ -32,10 +32,12 @@ public class UserController {
         if(users.size()==1){
             Cookie cookie = new Cookie("login","true");
             response.addCookie(cookie);
-
-            result.put("msg","成功");
+            result.put("rsp_code","0000");
+            result.put("rsp_msg","成功");
+            result.put("user",users.get(0));
         }else {
-            result.put("msg","用户名或密码不对");
+            result.put("rsp_code","1001");
+            result.put("rsp_msg","用户名或密码不对");
         }
         return gson.toJson(result);
     }
@@ -45,20 +47,22 @@ public class UserController {
     @RequestMapping(value = "saveUser",method = RequestMethod.POST)
     @ApiOperation(value = "添加/修改用户接口",notes="传入需要添加/修改的用户信息",httpMethod = "POST")
     public String  addUser(HttpServletRequest request,@RequestBody User user){
-        Boolean x = verifyCookies(request);
+        Boolean flag = verifyCookies(request);
 
         Map<String,Object> result=new HashMap<String,Object>();
 
-        if(x!=null){
+        if(flag){
             try {
                 userDao.save(user);
-                result.put("msg","成功");
-                result.put("data",user);
+                result.put("rsp_code","0000");
+                result.put("rsp_msg","成功");
             }catch (Exception e){
-                result.put("msg","数据异常,添加/修改失败");
+                result.put("rsp_code","9999");
+                result.put("msg","数据异常，注意检查参数");
             }
         }else{
-            result.put("msg","用户未登陆");
+            result.put("rsp_code","1000");
+            result.put("rsp_msg","用户未登陆");
         }
         return  gson.toJson(result);
     }
@@ -69,15 +73,17 @@ public class UserController {
     @ApiOperation(value = "用户详情接口",notes = "通过id查询用户的详细信息",httpMethod = "GET")
     public String getUserDetail(HttpServletRequest request, @RequestParam(value="user_id",required = true) Integer id){
 
-        Boolean x = verifyCookies(request);
+        Boolean flag = verifyCookies(request);
         Map<String,Object> result=new HashMap <>();
 
-        if(x==null){
+        if(flag){
             User user =userDao.getOne(id);
-            result.put("msg","成功");
+            result.put("rsp_code","0000");
+            result.put("rsp_msg","成功");
             result.put("data",user);
         }else{
-            result.put("msg","用户未登陆");
+            result.put("rsp_code","1000");
+            result.put("rsp_msg","用户未登陆");
         }
         return gson.toJson(result);
     }
@@ -87,13 +93,14 @@ public class UserController {
     @ApiOperation(value = "查询用户",notes="通过用户的name来查询用户",httpMethod = "GET")
     public String getUserListByName(HttpServletRequest request, @RequestBody User user){
 
-        Boolean x = verifyCookies(request);
+        Boolean flag = verifyCookies(request);
         Map<String,Object> result=new HashMap <>();
 
-        if(x==null){
+        if(flag){
             List <User> users =new ArrayList <>();
             users=userDao.findByName(user.getName());
-            result.put("msg","成功");
+            result.put("rsp_code","0000");
+            result.put("rsp_msg","成功");
             result.put("data",users);
         }
         return gson.toJson(result);
